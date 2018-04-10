a33444c62f29639a3f15e3b486679e6391703838
Timothy Tickle <timothyltickle@gmail.com>
2018-04-10 18:48:53 -0400

    Update predicted_separability.R

--- predicted_separability.R
+++ predicted_separability.R
@@ -5,9 +5,11 @@
 
 ################################ Arguments #####################################
 # args[1]    Path to loom file
-# args[2]    Fraction of cluster 1 in the full dataset
-# args[3]    Fraction of cluster 2 in the full dataset
-# args[4]    Path for output tsv file
+# args[2]    Name of cluster 1 
+# args[3]    Fraction of cluster 1 in the full dataset
+# args[4]    Name of cluster 2 
+# args[5]    Fraction of cluster 2 in the full dataset
+# args[6]    Path for output tsv file
 ################################################################################
 
 suppressMessages(library(Seurat))
@@ -31,12 +33,14 @@ seurat.object <- SetIdent(object = seurat.object,
 
 cluster.avgs <- AverageExpression(object = seurat.object, display.progress = FALSE)
 
+cluster_1_name = args[3]
+cluster_2_name = args[5]
 # Find DE genes as those with > log(2) average difference between the two clusters
-num.de.genes <- length(which(abs(cluster.avgs[, 1] - cluster.avgs[, 2]) > log(2)))
+num.de.genes <- length(which(abs(cluster.avgs[, cluster_1_name] - cluster.avgs[, cluster_2_name]) > log(2)))
 
 # Simple prediction based soley on DE genes
 num.cells <- seq(1000, 100000, 1000)
 predicted.separability <- 1 / (1 + exp(-num.cells * num.de.genes / 1e7))
 output <- data.frame(num.cells, predicted.separability)
 colnames(output) <- c("Number of Cells", "Predicted Separability")
-write.table(x = output, file = args[4], sep = "\t", quote = FALSE, row.names = FALSE)
+write.table(x = output, file = args[6], sep = "\t", quote = FALSE, row.names = FALSE)
